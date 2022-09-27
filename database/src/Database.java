12cbf763d6b723466aac6623c9e85ac13e186b96
Kyle <kyle@hoffpauir.com>
2022-09-27 10:02:32 -0400

    insert written but existing tables not correctly found. check that tables are being added to the db hashmap

--- database/src/Database.java
+++ database/src/Database.java
@@ -1,40 +1,52 @@
 import sun.font.TrueTypeFont;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Database {
-    private static final String CATALOG = "dbfiles";
+    private static final String CATALOG = "dbfiles.db";
     private static final String CATALOG_PATH = "dbfiles/";
 
     public static void main(String[] args) throws IOException {
-        //attempt to create the dbFiles if it doesnt already exist
-
-        File catalog = new File(CATALOG);
-        HashMap<String, String> db = new HashMap<String, String>();
-        try {
-            if (!catalog.exists()) {
-                catalog.mkdir();
-            }
-            else {
-                System.out.println("Reading db files...");
-                //read from the db files and store in some structure
-                db = readDbFiles(catalog);
-                System.out.println("Reading complete!");
-            }
-        } catch(Exception e) {
-            System.err.println("Error creatinng or reading from dbFiles: " + catalog.toString());
-            System.err.println(e);
-        }
+        // going to be pulled from dbFiles, mapping Tablesnames to columns and their size
+        HashMap<String, String[]> db = new HashMap<String, String[]>();
+        File catalog = load(db);
         //run the program now that the dbFiles loaded
         run(catalog, db);
     } // end main
 
+    private static File load(HashMap<String, String[]> db ) {
+        //attempt to create the dbFiles if it doesnt already exist
+        File catalog_dir = new File(CATALOG);
+        File catalog = null;
+        try {
+            if (!catalog_dir.exists()) {
+                catalog_dir.mkdir();
+                catalog = new File(CATALOG_PATH + CATALOG);
+                catalog.createNewFile();
+            }
+            else {
+                System.out.println("Reading db files...");
+                //read from the db files and store in some structure
+                catalog = new File(CATALOG_PATH + CATALOG);
+                db = readDbFiles(catalog);
+                System.out.println("Reading complete!");
+            }
+        } catch(Exception e) {
+            System.err.println("Error creating or reading from dbFiles: " + catalog_dir.toString());
+            System.err.println(e);
+            System.exit(1);
+        }
+        return catalog;
+    }
 
-    public static void run(File catalog, HashMap<String, String> db) throws IOException {
+
+    public static void run(File catalog, HashMap<String, String[]> db) throws IOException {
         //scanner object to prompt for user input and to pass to functions
         Scanner scanner = new Scanner(System.in);
         int option;
@@ -50,7 +62,7 @@ public class Database {
                     createTable(scanner);
                     break;
                 case 2:
-                    insert(scanner);
+                    insert(scanner, db);
                     break;
                 case 3:
                     remove(scanner);
@@ -88,8 +100,30 @@ public class Database {
 
 
     // read from the db files and store in some structure
-    public static HashMap<String, String> readDbFiles(File catalog) {
-        return null;
+    public static HashMap<String, String[]> readDbFiles(File catalog) {
+        HashMap<String, String[]> db = new HashMap<String, String[]>();
+        try {
+            Scanner file = new Scanner(catalog);
+            while(file.hasNextLine()) {
+                String[] line = file.nextLine().split(" ");
+                // get the table name from your dbFile
+                String table = line[0];
+                String[] cols = new String[line.length];
+                int count = 0;
+                // add your columns and their maximum size to the cols array
+                for (int i = 1; i < line.length - 1; i += 2) {
+                    cols[count] = line[i] + " " + line[i+1];
+                    count++;
+                }
+                // add your column info to your table -> col map
+                db.put(table, cols);
+            }
+        } catch(FileNotFoundException e) {
+            System.err.println("Could not find file " + catalog.getAbsolutePath());
+        }
+        // db will now hold (for example)
+        // BOOKS -> ["author 20", "title 25", "ISBN 15"]
+        return db;
     }
 
 
@@ -101,8 +135,9 @@ public class Database {
         boolean keepReading = true;
 
         //if table already exists, kick user out
-        System.out.println("Enter table name");
+        System.out.print("Enter table name: ");
         String tableName = in.nextLine();
+        System.out.println();
         File table = new File(CATALOG_PATH + tableName + ".db");
         if (table.exists()) {
             System.err.println("Error! cannot create table " + tableName + ".db as it already exists!");
@@ -111,13 +146,25 @@ public class Database {
 
         //take in and store column info
         while(keepReading) {
-            System.out.println("Enter column name and it's length:");
+            System.out.print("Enter column name and it's length: ");
             String line = in.nextLine();
+            System.out.println();
             if(line.equals(""))
                 keepReading = false;
             else {
-                colNames.add(line.split(" ")[0]);
-                colLengths.add(Integer.parseInt(line.split(" ")[1]));
+                String[] lineSplit = line.split(" ");
+                String name = lineSplit[0];
+                int size = Integer.parseInt(line.split(" ")[1]);
+                // ensure single word colNames
+                if( name.contains(" ")) {
+                    System.err.println("Error! Db columns must be single words");
+                    table.delete();
+                    System.err.println("Table deleted.");
+                    return;
+                }
+                // add to lists
+                colNames.add(name);
+                colLengths.add(size);
             }
 
         }
@@ -133,22 +180,79 @@ public class Database {
     }
 
 
+
     private static void dump(File table, ArrayList<String> colNames, ArrayList<Integer> colLengths) throws IOException {
         //dump data to disk
         table.createNewFile();
-        FileWriter fw = new FileWriter(table);
+        File catalog = new File(CATALOG_PATH + CATALOG);
+        // create writes for both the table file and catalog file
+        FileWriter fwTable = new FileWriter(table);
+        FileWriter fwCatalog = new FileWriter(catalog);
+        fwCatalog.append(table.getName() + "\t");
         for(int i = 0; i < colNames.size(); i++) {
-            fw.append(colNames.get(i) + " ");
-            fw.append(colLengths.get(i).toString() + "\t");
+            // add column info to catalog for later size checking
+            fwCatalog.append(colNames.get(i) + " ");
+            fwCatalog.append(colLengths.get(i).toString() + "\t");
+            // add column names to the table
+            fwTable.append(colNames.get(i) + " ");
         }
-        fw.close();
+        fwCatalog.append("\n");
+        fwTable.append("\n");
+        fwTable.close();
+        fwCatalog.close();
         return;
     }
 
 
     //insert into an existing table
-    public static void insert(Scanner in) {
+    public static void insert(Scanner in, HashMap<String, String[]> db) throws IOException {
+        System.out.print("Enter file name: ");
+        String table = in.nextLine();
+        FileWriter fw = null;
+        try {
+            if ((db.get(table) == null))
+                throw new IOException();
+            fw = new FileWriter(CATALOG_PATH + table + ".db");
+        } catch (IOException e) {
+            System.err.println("Error - table does not exist.");
+            return;
+        }
 
+        String[] tableData = db.get(table);
+        ArrayList<String> newEntries = new ArrayList<String>();
+        //loop while the user wants to input more data
+        boolean moreEntries = true;
+        while(moreEntries ) {
+            //go through every col and prompt for input
+            for (String i : tableData) {
+                String[] splitCol = i.split(" ");
+                String colName = splitCol[0];
+                int colSize = Integer.parseInt(splitCol[1]);
+                System.out.print(colName + ": ");
+                String col = in.nextLine();
+                //ensure that user input is of proper size
+                if (col.length() > colSize) {
+                    System.err.println("Error - input data exceeds size for column");
+                    return;
+                }
+                newEntries.add(col);
+                System.out.println();
+            }
+            //append new entries to file
+            for ( String x : newEntries) {
+                fw.append(x + "\t");
+            }
+            fw.append("\n");
+            //ask user if they'd like to add more entries
+            System.out.print("More entries (Y / n): ");
+            String more = in.nextLine().toLowerCase();
+            System.out.println();
+            if(!more.equals("y")) {
+                moreEntries = false;
+            }
+        }//end moreEntries loop
+        fw.close();
+        return;
     }
 
 
@@ -165,7 +269,7 @@ public class Database {
 
 
     //delete entries from table marked by the remove function
-    public static void purge(File catalog, HashMap<String, String> db) {
+    public static void purge(File catalog, HashMap<String, String[]> db) {
 
     }
 
